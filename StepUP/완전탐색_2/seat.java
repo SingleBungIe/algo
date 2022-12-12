05d5fd3e7cbcb9a7c6791431e759063b83e70e3a
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2022-12-12 23:50:55 +0900

    사방탐색

--- StepUP/완전탐색_2/seat.java
+++ StepUP/완전탐색_2/seat.java
@@ -3,28 +3,50 @@ package StepUP.완전탐색_2;
 import java.util.Scanner;
 
 public class seat {
-    static int dy[] = {1, 0, -1, 0};
-    static int dx[] = {0, 1, 0, -1};
+    // 4방 탐색 일반화
+    static int dr[] = {0, 1, 0, -1};
+    static int dc[] = {1, 0, -1, 0};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int R = sc.nextInt();
         int C = sc.nextInt();
-        int seat[][] = new int[C + 1][R + 1];
-        int people = 1;
-        int x = 0;
-        int y = 0;
+        int seatNum = sc.nextInt();
+        int seat[][] = new int[R][C];
+        int r = 0;
+        int c = 0;
+        int nr = 0;
+        int nc = 0;
+        int cnt = 1;
+        int dir = 0;
 
-        while (true) {
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (0 < nx || R < R) {
+        seat[0][0] = cnt++;
+        while (cnt - 1 != R * C) {
+            nr = r + dr[dir];
+            nc = c + dc[dir];
 
-                }
+
+            if (nr < 0 || nr == R || nc < 0 || nc == C || seat[nr][nc] != 0) {
+                dir++;
+                if (dir == 4)
+                    dir = 0;
+                nr = r + dr[dir];
+                nc = c + dc[dir];
 
             }
-        }
 
+            seat[nr][nc] = cnt++;
+            if (cnt - 1 == seatNum) {
+                System.out.println((nr + 1) + " " + (nc + 1));
+                return;
+            }
+            r = nr;
+            c = nc;
+
+        }
+        if (R * C < seatNum)
+            System.out.println("0");
     }
+
 }
+
