cc16b8e5bd3d0e913f27ab6b88a1e5155ea6014e
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2024-01-09 19:05:11 +0900

    삼성전자 알고리즘 특강 사전문제

--- New_D1/D1_2056_연월일_달력.java
+++ New_D1/D1_2056_연월일_달력.java
@@ -1,7 +1,10 @@
 package New_D1;
 
+import java.util.Scanner;
+
 public class D1_2056_연월일_달력 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
     }
 }
