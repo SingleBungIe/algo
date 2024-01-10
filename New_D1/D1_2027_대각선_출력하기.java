64ab248ee53950d63b28491eb2d6d4a2ddd7586a
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2024-01-10 17:58:36 +0900

    D1

---
+++ New_D1/D1_2027_대각선_출력하기.java
@@ -0,0 +1,11 @@
+package New_D1;
+
+public class D1_2027_대각선_출력하기 {
+    public static void main(String[] args) {
+        System.out.println("#++++\n" +
+                "+#+++\n" +
+                "++#++\n" +
+                "+++#+\n" +
+                "++++#");
+    }
+}
