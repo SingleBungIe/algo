5f272334d021b8572a3e895c5b23415c4ab24e70
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2024-01-16 20:31:01 +0900

    D2

--- SWEA/New_D2/D2_2005_파스칼의_삼각형.java
+++ SWEA/New_D2/D2_2005_파스칼의_삼각형.java
@@ -3,9 +3,5 @@ package SWEA.New_D2;
 public class D2_2005_파스칼의_삼각형 {
     public static void main(String[] args) {
         System.out.println("");
-        System.out.println("");
-        System.out.println("D2");
-        System.out.println("D2");
-        System.out.println("D2");
     }
 }
